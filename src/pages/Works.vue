be0bae0ccdfccdcd85516c04445477ae364b7fa8
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2023-01-09 20:24:53 +0100

    add links

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -4,20 +4,27 @@
     </div>
     <div class="works-scroller snaps-inline">
         <div class="works-element">
-            <img src="../assets/works/mcstackcalc.png" alt="Project">
-            <h3>Minecraft Stack Calculator</h3>
+            <a href="https://mcstackcalc.netlify.app/"><img src="../assets/works/mcstackcalc.png" alt="Project">
+                <h3>Minecraft Stack Calculator</h3>
+            </a>
         </div>
         <div class="works-element">
-            <img src="../assets/works/website.png" alt="Project">
-            <h3>This Website!</h3>
+            <a href="https://github.com/Nelertile/nelertile.github.io">
+                <img src="../assets/works/website.png" alt="Project">
+                <h3>This Website!</h3>
+            </a>
         </div>
         <div class="works-element">
-            <img src="../assets/works/sine.png" alt="Project">
-            <h3>Sine - A roller coaster with 3 inversions</h3>
+            <a href="https://youtu.be/NKF9Uzsh1aY">
+                <img src="../assets/works/sine.png" alt="Project">
+                <h3>Sine - A roller coaster with 3 inversions</h3>
+            </a>
         </div>
         <div class="works-element">
-            <img src="../assets/works/fcoaster.png" alt="Project">
-            <h3>First Coaster - My first coaster in NL2</h3>
+            <a href="https://youtu.be/jryUgxoLAzs">
+                <img src="../assets/works/fcoaster.png" alt="Project">
+                <h3>First Coaster - My first coaster in NL2</h3>
+            </a>
         </div>
     </div>
     <div class="title">
